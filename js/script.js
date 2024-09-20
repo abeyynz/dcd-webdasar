e40513a15474d79d2c03d31cc4ef68e8495c163c
abiyyatun nazihah <abiyyatunnazihah@gmail.com>
2024-09-20 10:04:01 +0700

    Menghapus bagian section about

--- js/script.js
+++ js/script.js
@@ -1,23 +1,52 @@
-function biodata(personId) {
-    const article = document.getElementById('bio');
+function showBiodata(personId) {
+    const articleTitle = document.getElementById('bio-title');
+    const articleImg = document.getElementById('bio-img');
+    const articleDetails = document.getElementById('bio-details');
     
     if (personId === 1) {
-        article.innerHTML = `
-            <h1>Sungho Profile</h1>
+        articleTitle.innerText = "Sungho Profile";
+        articleImg.src = "img/foto1.jpg";
+        articleDetails.innerHTML = `
             <p>Nama: Orang Pertama</p>
             <p>Tanggal Lahir: 1 Januari 1990</p>
             <p>Pekerjaan: Penyanyi</p>
         `;
     } else if (personId === 2) {
-        article.innerHTML = `
-            <h1>Riwoo Profile</h1>
+        articleTitle.innerText = "Riwoo Profile";
+        articleImg.src = "img/foto2.jpg";
+        articleDetails.innerHTML = `
             <p>Nama: Orang Kedua</p>
             <p>Tanggal Lahir: 2 Februari 1992</p>
             <p>Pekerjaan: Rapper</p>
         `;
     } else if (personId === 3) {
-        article.innerHTML = `
-            <h1>Jaaehyun Profile</h1>
+        articleTitle.innerText = "Jaehyun Profile";
+        articleImg.src = "img/foto3.jpg";
+        articleDetails.innerHTML = `
+            <p>Nama: Orang Ketiga</p>
+            <p>Tanggal Lahir: 3 Maret 1995</p>
+            <p>Pekerjaan: Penari</p>
+        `;
+    } else if (personId ===4) {
+        articleTitle.innerText = "Taesan Profile";
+        articleImg.src = "img/foto4.jpg";
+        articleDetails.innerHTML = `
+            <p>Nama: Orang Ketiga</p>
+            <p>Tanggal Lahir: 3 Maret 1995</p>
+            <p>Pekerjaan: Penari</p>
+        `;
+    } else if (personId === 5) {
+        articleTitle.innerText = "Leehan Profile";
+        articleImg.src = "img/foto5.jpg";
+        articleDetails.innerHTML = `
+            <p>Nama: Orang Ketiga</p>
+            <p>Tanggal Lahir: 3 Maret 1995</p>
+            <p>Pekerjaan: Penari</p>
+        `;
+    } else if (personId === 6) {
+        articleTitle.innerText = "Woonhak Profile";
+        articleImg.src = "img/foto6.jpg";
+        articleDetails.innerHTML = `
             <p>Nama: Orang Ketiga</p>
             <p>Tanggal Lahir: 3 Maret 1995</p>
             <p>Pekerjaan: Penari</p>
